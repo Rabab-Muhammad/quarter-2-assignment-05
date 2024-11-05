fd6642c5d08a3a3376f8fd36ab4ba2249f8754d5
Rabab-Muhammad <rababmuhammadhussain@gmail.com>
2024-11-05 12:17:41 +0500

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,29 @@ export default function Home() {
     <div className="h-screen">
       <Header />
       <div className="flex h-[80%]">
-        <div className="w-1/2 flex flex-col justify-center items-start m-24  ">
+        <div className="w-1/2 flex flex-col justify-center items-start m-24 pt-20 ">
           <h1 className="text-[40px] font-bold font-serif text-[#000000] w-[496px]  tracking-wide ">
             IMPECCABLE CRAFTSMANSHIP AND FINESSE
           </h1>
-          <p className="mt-5">
+          <p className="pt-5">
             <Image
-            src={"/images/para.svg"}
-            alt="paragraph" 
-            height={147}
-            width={902} 
+              src={"/images/para.svg"}
+              alt="paragraph"
+              height={147}
+              width={902}
             />
           </p>
-          <button className="bg-[#A29875] p-[10px] rounded-[10px] w-[288px] h-[58px] text-2xl text-white font-serif  mt-10 ">Explore Now</button>
+          <button className="bg-[#A29875] p-[10px] rounded-[10px] w-[288px] h-[58px] text-2xl text-white font-serif  mt-8 ">
+            Explore Now
+          </button>
         </div>
-        <div className="w-1/2 flex justify-center mt-10">
+        <div className="w-1/2 flex justify-center pt-5">
           <Image
-          src={"/images/figma.svg"}
-          alt="main-image"
-          height={647}
-          width={462}/>
+            src={"/images/figma.svg"}
+            alt="main-image"
+            height={647}
+            width={462}
+          />
         </div>
       </div>
     </div>
